38520122ae594777255d9ffc49266db26b508bce
Phoomparin Mano <poom@hey.com>
2021-08-08 01:58:13 +0700

    refactor: isPromise check and flatten type

--- src/structure.ts
+++ src/structure.ts
@@ -1,5 +1,3 @@
-import {isCallSignatureDeclaration} from 'typescript'
-
 // Data-Driven
 interface IDateRange {
   from: Date | string
@@ -24,7 +22,8 @@ interface IAgenda {
   slots: IAgendaSlot[]
 }
 
-interface IEvent {
+// Root State
+interface IEventState {
   detail: IEventDetail
   agenda: IAgenda
 }
@@ -42,7 +41,7 @@ const timeslot = (from: string, to: string): IDateRange => ({
   to: time(to),
 })
 
-const event2: IEvent = {
+const event2: IEventState = {
   detail: {
     title: 'GraphQL Meetup 10.0',
     location: 'San Francisco, CA',
@@ -106,6 +105,9 @@ interface Store<S, E, E2 = E & SysEvents> {
   on: OnFn<S, E2>
 }
 
+const isPromise = <T>(data: MaybeAsync<T>): data is Promise<T> =>
+  data && 'then' in data && typeof data.then === 'function'
+
 export const createStore = <S, E extends IMap>(initialState: S) => {
   let state = initialState
   let events: HandlersMap<S, E> = {}
@@ -121,15 +123,16 @@ export const createStore = <S, E extends IMap>(initialState: S) => {
       events = {...events, [event]: [...handlers, handler]}
     },
 
-    async run(event, data) {
+    run(event, data) {
       const handlers = events[event] ?? []
       if (handlers.length === 0) return
 
       for (const handler of handlers) {
-        const nextState = await handler(state, data)
-        if (nextState === undefined) return
+        const nextState = handler(state, data)
+        if (nextState === undefined) continue
+        if (isPromise(nextState)) continue
 
-        state = nextState
+        store.set(nextState)
       }
 
       if (event !== '@run') {
@@ -148,6 +151,12 @@ export const createStore = <S, E extends IMap>(initialState: S) => {
 }
 
 const store = createStore<IAgenda, IAction>({slots: []})
+store.on('@run', (state, data) => {
+  data //?
+})
+
+store.on('agenda/add', (state, slot) => ({slots: [...state.slots, slot]}))
+store.run('agenda/add', {title: 'OK', time: timeslot('19:00', '19:10')})
 
 store.state //?
 
@@ -161,4 +170,39 @@ const module: Module<IAgenda, IAction> = {
   on: {
     'agenda/add': (state, data) => state,
   },
+}
+
+interface BookEvents {
+  borrow: string
+  return: string
+  donate: string
+}
+
+interface UserEvents {
+  login: string
+}
+
+interface IRootEvents {
+  book: BookEvents
+  user: UserEvents
+}
+
+type PrefixWithKey<T, K extends string> = {
+  [P in keyof T as `${K}/${P extends string ? P : never}`]: T[P]
+}
+
+type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends (
+  k: infer I
+) => void
+  ? I
+  : never
+
+type Result = {[K in keyof IRootEvents]: PrefixWithKey<IRootEvents[K], K>}
+type Result2 = UnionToIntersection<Result[keyof Result]>
+
+const flattened: Result2 = {
+  'book/borrow': '',
+  'book/donate': '',
+  'book/return': '',
+  'user/login': '',
 }
