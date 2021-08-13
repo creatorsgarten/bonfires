42b5834b3029875acaa534c5dc50a1830541dcca
Phoomparin Mano <poom@hey.com>
2021-08-14 04:21:11 +0700

    chore: cleanup experiments

--- packages/experiments/v1/extensions/index.ts
+++
@@ -1,5 +0,0 @@
-export {NotionExtension} from './notion'
-export type {INotionContext} from './notion/context'
-
-export {ScheduleExtension} from './schedule'
-export {CongratulateExtension} from './congratulate'
--- packages/experiments/v1/utils/promise.ts
+++
@@ -1,4 +0,0 @@
-import type {MaybeAsync} from '../@types/IStore'
-
-export const isPromise = <T>(data: MaybeAsync<T>): data is Promise<T> =>
-  data && 'then' in data && typeof data.then === 'function'
