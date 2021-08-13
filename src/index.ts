5310e390e3a1535374cc8c81cd0e6651d50cb7a5
Phoomparin Mano <poom@hey.com>
2021-08-14 04:18:34 +0700

    chore: move createEvent utility

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ export {EventBus} from './core/module/EventBus'
 export {DynamicRegistry} from './core/module/DynamicRegistry'
 export {Registry, createRegistry} from './core/module/Registry'
 
-export {createEvent} from './core/event/Event'
+export {createEvent} from './utils/createEvent'
 
 export {DuplicateModuleError} from './errors/DuplicateExtensionError'
 
