3b9e0d38b7451b183513ef008ad545867edda216
Phoomparin Mano <poom@hey.com>
2021-12-04 11:37:15 +0700

    chore: responsive

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -4,7 +4,10 @@ const Container = tw.section`
 	flex justify-center items-center min-h-screen bg-gray-50
 `
 
-const Title = tw.h1`text-5xl font-extralight text-gray-800`
+const Title = tw.h1`
+	text-2xl sm:text-4xl font-extralight
+	text-gray-800 text-center
+`
 
 export function Dashboard() {
   return (
