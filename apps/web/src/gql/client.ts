2f5a03cd2cd5494d44cd20086daf764a83421ece
Phoomparin Mano <poom@hey.com>
2021-12-05 02:37:46 +0700

    feat: type policies

--- apps/web/src/gql/client.ts
+++ apps/web/src/gql/client.ts
@@ -1,8 +1,25 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
+import { TypedTypePolicies } from '@gql'
+
+const typePolicies: TypedTypePolicies = {
+  Query: {
+    fields: {
+      event: {
+        read: (_, { args, toReference }) =>
+          toReference({ __typename: 'Event', id: args?.id }),
+      },
+    },
+  },
+}
+
 export function createApolloClient() {
+  const cache = new InMemoryCache({
+    typePolicies,
+  })
+
   return new ApolloClient({
     uri: '/api/graphql',
-    cache: new InMemoryCache(),
+    cache,
   })
 }
