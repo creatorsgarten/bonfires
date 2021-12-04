fa2352f524fc1ded8aa2f5bbaf7398130213cbbb
Phoomparin Mano <poom@hey.com>
2021-12-05 02:13:47 +0700

    chore: transition

--- apps/web/src/modules/event-dashboard/EventDashboardView.tsx
+++ apps/web/src/modules/event-dashboard/EventDashboardView.tsx
@@ -16,7 +16,7 @@ export const EventDashboardView = () => {
 
   return (
     <div
-      tw="flex justify-center items-center min-h-screen bg-blackrussian text-7xl font-semibold text-white text-center break-all"
+      tw="flex justify-center items-center min-h-screen bg-gray-50 text-7xl font-semibold text-white text-center break-all"
       css={{ background: event?.color ?? '' }}
     >
       {event?.name}
