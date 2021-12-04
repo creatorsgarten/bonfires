4cb317e5ebfa36fccf2a6169781b62981b6a53d9
Phoomparin Mano <poom@hey.com>
2021-12-04 09:30:56 +0700

    chore: prettier formatting

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,22 +1,18 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
+import { Logger } from '@nestjs/common'
+import { NestFactory } from '@nestjs/core'
 
-import { Logger } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-
-import { AppModule } from './app/app.module';
+import { AppModule } from './app/app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
+  const app = await NestFactory.create(AppModule)
+
+  const prefix = 'api'
+  app.setGlobalPrefix(prefix)
+
+  const port = process.env.PORT || 3333
+  await app.listen(port)
+
+  Logger.log(`🚀 Application is running on: http://localhost:${port}/${prefix}`)
 }
 
-bootstrap();
+bootstrap()
