c05fbb48062d9b6923af1ab63cf165ab35450f09
Phoomparin Mano <poom@hey.com>
2022-04-02 01:25:09 +0700

    feat: responsive text

--- apps/web/src/modules/duty-view/index.tsx
+++ apps/web/src/modules/duty-view/index.tsx
@@ -7,15 +7,15 @@ export const DutyView = () => {
 
   return (
     <div tw="flex justify-center items-center min-h-screen font-semibold text-white text-center break-all bg-gray-800">
-      <div tw="mx-auto w-full max-w-2xl px-10 space-y-10">
-        <div tw="flex flex-col text-gray-800 md:text-2xl font-light">
+      <div tw="mx-auto w-full max-w-2xl px-4 sm:px-10 space-y-10">
+        <div tw="flex flex-col text-gray-800 xs:text-xl sm:text-2xl font-light">
           <div tw="flex items-center justify-around px-3 py-2 bg-white rounded-t-md">
             <div>00:00:00</div>
             <div>
-              <span tw="text-xs md:text-xl">คิว</span> 00
+              <span tw="text-xs sm:text-xl">คิว</span> 00
             </div>
             <div>
-              <span tw="text-xs md:text-xl">เหลือ</span> 00:00
+              <span tw="text-xs sm:text-xl">เหลือ</span> 00:00
             </div>
           </div>
 
@@ -23,14 +23,16 @@ export const DutyView = () => {
         </div>
 
         <div tw="flex flex-col text-gray-800 shadow-2xl">
-          <div tw="px-2 py-4 bg-white rounded-t-md text-2xl md:text-3xl">
+          <div tw="px-2 py-4 bg-white rounded-t-md text-2xl sm:text-3xl">
             <div tw="text-center w-full">นอนหลับพักผ่อน</div>
           </div>
 
-          <div tw="rounded-b-lg text-white px-2 py-2 text-sm md:text-base">
+          <div tw="rounded-b-lg text-white px-2 py-2 text-xs xs:text-sm sm:text-base">
             ต่อไป รับน้องมาจากโรงแรม · ช่วง chaos
           </div>
         </div>
+
+        <div tw="text-xs text-gray-300">{event?.name}</div>
       </div>
     </div>
   )
