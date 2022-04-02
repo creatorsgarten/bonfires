7ed444eceb398b7b2df4580693664884da3a5d2d
Phoomparin Mano <poom@hey.com>
2022-04-02 22:58:03 +0700

    feat: show displayName in title

--- apps/web/src/modules/duty-card/AssigneeAvatar.tsx
+++ apps/web/src/modules/duty-card/AssigneeAvatar.tsx
@@ -3,7 +3,7 @@ import 'twin.macro'
 import { Staff, User } from '@gql'
 
 export type Assignee = Pick<Staff, 'id' | 'displayName'> & {
-  user: Pick<User, 'photo'>
+  user: Pick<User, 'photo' | 'displayName'>
 }
 
 interface Props {
@@ -19,6 +19,7 @@ export const AssigneeAvatars = ({ assignees }: Props) => {
         {assignees?.map((assignee) => (
           <div
             key={assignee.id}
+            title={assignee.displayName ?? assignee.user.displayName ?? 'Staff'}
             style={{ backgroundImage: `url(${assignee.user.photo})` }}
             tw="bg-gray-700 flex w-7 h-7 rounded-full shadow-2xl border-white border-2 border-solid bg-cover"
           />
