d6de7ec46e5ccd8f0950f2a13509746e004e0df6
Phoomparin Mano <poom@hey.com>
2022-04-02 17:12:06 +0700

    feat: remaining time calculation

--- apps/web/__tests__/time-slot.test.ts
+++ apps/web/__tests__/time-slot.test.ts
@@ -1,5 +1,10 @@
 import { DateTime } from 'luxon'
-import { slotFromTime, timeFromSlot } from '../src/utils/slot'
+
+import {
+  slotFromTime,
+  timeFromSlot,
+  remainingTimeInSlot,
+} from '../src/utils/slot'
 
 const starts = '2022-04-02T09:30:00.000+07:00'
 
@@ -8,8 +13,10 @@ describe('time slot calculation', () => {
     [0, '09:30'],
     [1, '09:40'],
     [5, '10:20'],
-  ])('can compute the time at the given slot', (slot, expected) => {
-    expect(timeFromSlot(slot, starts)).toBe(expected)
+  ])('shows the time from the given slot', (slot, expected) => {
+    const time = timeFromSlot(slot, starts)
+
+    expect(time.toLocaleString(DateTime.TIME_24_SIMPLE)).toBe(expected)
   })
 
   it.each([
@@ -19,9 +26,22 @@ describe('time slot calculation', () => {
     ['09:47', 1],
     ['10:04', 3],
     ['20:15', 64],
-  ])('can compute the slot at the given time', (nowTime, slot) => {
-    const now = DateTime.fromISO(`2022-04-02T${nowTime}:00.000+07:00`)
+  ])('shows the slot from the given time', (timeNow, slot) => {
+    const now = DateTime.fromISO(`2022-04-02T${timeNow}:00.000+07:00`)
 
     expect(slotFromTime(starts, 10, now)).toBe(slot)
+  })
+
+  it.each([
+    ['09:30:00', '10:00'],
+    ['09:30:30', '09:30'],
+    ['09:35:00', '05:00'],
+    ['09:39:50', '00:10'],
+    ['22:00:55', '09:05'],
+  ])('shows the remaining time in the current slot', (timeNow, expected) => {
+    const now = DateTime.fromISO(`2022-04-02T${timeNow}.000+07:00`)
+    const remaining = remainingTimeInSlot(starts, 10, now).toFormat('mm:ss')
+
+    expect(remaining).toBe(expected)
   })
 })
--- apps/web/src/utils/slot.ts
+++ apps/web/src/utils/slot.ts
@@ -4,10 +4,7 @@ export const timeFromSlot = (
   slot: number,
   startsAt: string,
   slotDurationInMinutes = 10
-) =>
-  DateTime.fromISO(startsAt)
-    .plus({ minutes: slot * slotDurationInMinutes })
-    .toFormat('hh:mm')
+) => DateTime.fromISO(startsAt).plus({ minutes: slot * slotDurationInMinutes })
 
 export const slotFromTime = (
   startsAt: string,
@@ -19,4 +16,15 @@ export const slotFromTime = (
   const slot = Math.floor(diff.minutes / slotDurationInMinutes)
 
   return Math.max(slot, 0)
+}
+
+export const remainingTimeInSlot = (
+  startsAt: string,
+  slotDurationInMinutes = 10,
+  currentTime = DateTime.now()
+) => {
+  const slot = slotFromTime(startsAt, slotDurationInMinutes, currentTime)
+  const nextSlot = timeFromSlot(slot + 1, startsAt, slotDurationInMinutes)
+
+  return nextSlot.diff(currentTime, ['minutes'])
 }
