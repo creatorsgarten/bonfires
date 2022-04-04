263e799c2d1ebb99e2cadd6a7651051120260927
Phoomparin Mano <poom@hey.com>
2022-04-04 14:30:31 +0700

    feat: get next non-empty agenda

--- apps/web/src/utils/agenda.ts
+++ apps/web/src/utils/agenda.ts
@@ -3,11 +3,15 @@ import { Agenda } from '@gql'
 export function agendaFromSlot(slot: number, agendas: Agenda[]) {
   agendas = [...agendas].sort((a, b) => a?.slot - b?.slot)
 
-  let i = -1
+  let now = -1
 
   for (let j = 0; j < agendas.length; j++) {
-    if (agendas[j].slot <= slot) i = j
+    if (!agendas[j].title) continue
+
+    if (agendas[j].slot <= slot) now = j
   }
 
-  return { current: agendas[i], next: agendas[i + 1] }
+  const next = agendas.slice(now + 1).find((a) => a.title)
+
+  return { current: agendas[now], next }
 }
